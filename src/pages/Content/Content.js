a9f30212f310c5d81a5fa5ec7eb419bc1758e9ef
Admin <19130112@st.hcmuaf.edu.vn>
2023-04-29 22:59:36 +0700

    update car item

---
+++ src/pages/Content/Content.js
@@ -0,0 +1,172 @@
+import React from "react";
+import clsx from "clsx";
+import styles from "./Content.module.scss"
+import {FaPaypal, FaShoppingBag} from "react-icons/fa";
+import {BiDetail} from "react-icons/bi";
+
+function Content() {
+    return (
+        <div className={clsx(styles.body)}>
+            <div className={clsx(styles.containerCar)}>
+                <div className={clsx(styles.carBoxItem)}>
+                    <div className={clsx(styles.imagesCar)}>
+                        <img
+                            src={"https://assets.ucars.sg/image/upload/c_fit,f_auto,w_1080/v1/s3/images/12956055851658307204616.PNG"}
+                        />
+                        <div className={clsx(styles.optionCar)}>
+                            <div className={clsx(styles.pay)}>
+                                <FaPaypal/>
+                            </div>
+                            <div className={clsx(styles.bag)}>
+                                <FaShoppingBag/>
+                            </div>
+                            <div className={clsx(styles.detail)}>
+                                <BiDetail/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={clsx(styles.nameCar, styles.paddingItemCar)}>BMW X3</div>
+                    <div className={clsx(styles.priceCar, styles.paddingItemCar)}>From <span>$248000</span></div>
+                    <div className={clsx(styles.desCar, styles.paddingItemCar)}>4 variants | with COE</div>
+                    <div className={clsx(styles.brand, styles.paddingItemCar)}>
+                        <img
+                            src={"https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/BMW.svg/2048px-BMW.svg.png"}/>
+                        <span>Performance Motos</span>
+                    </div>
+                </div>
+                <div className={clsx(styles.carBoxItem)}>
+                    <div className={clsx(styles.imagesCar)}>
+                        <img
+                            src={"https://assets.ucars.sg/image/upload/c_fit,f_auto,w_1080/v1/s3/images/12956055851658307204616.PNG"}
+                        />
+                        <div className={clsx(styles.optionCar)}>
+                            <div className={clsx(styles.pay)}>
+                                <FaPaypal/>
+                            </div>
+                            <div className={clsx(styles.bag)}>
+                                <FaShoppingBag/>
+                            </div>
+                            <div className={clsx(styles.detail)}>
+                                <BiDetail/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={clsx(styles.nameCar, styles.paddingItemCar)}>BMW X3</div>
+                    <div className={clsx(styles.priceCar, styles.paddingItemCar)}>From <span>$248000</span></div>
+                    <div className={clsx(styles.desCar, styles.paddingItemCar)}>4 variants | with COE</div>
+                    <div className={clsx(styles.brand, styles.paddingItemCar)}>
+                        <img
+                            src={"https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/BMW.svg/2048px-BMW.svg.png"}/>
+                        <span>Performance Motos</span>
+                    </div>
+                </div>
+                <div className={clsx(styles.carBoxItem)}>
+                    <div className={clsx(styles.imagesCar)}>
+                        <img
+                            src={"https://assets.ucars.sg/image/upload/c_fit,f_auto,w_1080/v1/s3/images/12956055851658307204616.PNG"}
+                        />
+                        <div className={clsx(styles.optionCar)}>
+                            <div className={clsx(styles.pay)}>
+                                <FaPaypal/>
+                            </div>
+                            <div className={clsx(styles.bag)}>
+                                <FaShoppingBag/>
+                            </div>
+                            <div className={clsx(styles.detail)}>
+                                <BiDetail/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={clsx(styles.nameCar, styles.paddingItemCar)}>BMW X3</div>
+                    <div className={clsx(styles.priceCar, styles.paddingItemCar)}>From <span>$248000</span></div>
+                    <div className={clsx(styles.desCar, styles.paddingItemCar)}>4 variants | with COE</div>
+                    <div className={clsx(styles.brand, styles.paddingItemCar)}>
+                        <img
+                            src={"https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/BMW.svg/2048px-BMW.svg.png"}/>
+                        <span>Performance Motos</span>
+                    </div>
+                </div>
+                <div className={clsx(styles.carBoxItem)}>
+                    <div className={clsx(styles.imagesCar)}>
+                        <img
+                            src={"https://assets.ucars.sg/image/upload/c_fit,f_auto,w_1080/v1/s3/images/12956055851658307204616.PNG"}
+                        />
+                        <div className={clsx(styles.optionCar)}>
+                            <div className={clsx(styles.pay)}>
+                                <FaPaypal/>
+                            </div>
+                            <div className={clsx(styles.bag)}>
+                                <FaShoppingBag/>
+                            </div>
+                            <div className={clsx(styles.detail)}>
+                                <BiDetail/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={clsx(styles.nameCar, styles.paddingItemCar)}>BMW X3</div>
+                    <div className={clsx(styles.priceCar, styles.paddingItemCar)}>From <span>$248000</span></div>
+                    <div className={clsx(styles.desCar, styles.paddingItemCar)}>4 variants | with COE</div>
+                    <div className={clsx(styles.brand, styles.paddingItemCar)}>
+                        <img
+                            src={"https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/BMW.svg/2048px-BMW.svg.png"}/>
+                        <span>Performance Motos</span>
+                    </div>
+                </div>
+                <div className={clsx(styles.carBoxItem)}>
+                    <div className={clsx(styles.imagesCar)}>
+                        <img
+                            src={"https://assets.ucars.sg/image/upload/c_fit,f_auto,w_1080/v1/s3/images/12956055851658307204616.PNG"}
+                        />
+                        <div className={clsx(styles.optionCar)}>
+                            <div className={clsx(styles.pay)}>
+                                <FaPaypal/>
+                            </div>
+                            <div className={clsx(styles.bag)}>
+                                <FaShoppingBag/>
+                            </div>
+                            <div className={clsx(styles.detail)}>
+                                <BiDetail/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={clsx(styles.nameCar, styles.paddingItemCar)}>BMW X3</div>
+                    <div className={clsx(styles.priceCar, styles.paddingItemCar)}>From <span>$248000</span></div>
+                    <div className={clsx(styles.desCar, styles.paddingItemCar)}>4 variants | with COE</div>
+                    <div className={clsx(styles.brand, styles.paddingItemCar)}>
+                        <img
+                            src={"https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/BMW.svg/2048px-BMW.svg.png"}/>
+                        <span>Performance Motos</span>
+                    </div>
+                </div>
+                <div className={clsx(styles.carBoxItem)}>
+                    <div className={clsx(styles.imagesCar)}>
+                        <img
+                            src={"https://assets.ucars.sg/image/upload/c_fit,f_auto,w_1080/v1/s3/images/12956055851658307204616.PNG"}
+                        />
+                        <div className={clsx(styles.optionCar)}>
+                            <div className={clsx(styles.pay)}>
+                                <FaPaypal/>
+                            </div>
+                            <div className={clsx(styles.bag)}>
+                                <FaShoppingBag/>
+                            </div>
+                            <div className={clsx(styles.detail)}>
+                                <BiDetail/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={clsx(styles.nameCar, styles.paddingItemCar)}>BMW X3</div>
+                    <div className={clsx(styles.priceCar, styles.paddingItemCar)}>From <span>$248000</span></div>
+                    <div className={clsx(styles.desCar, styles.paddingItemCar)}>4 variants | with COE</div>
+                    <div className={clsx(styles.brand, styles.paddingItemCar)}>
+                        <img
+                            src={"https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/BMW.svg/2048px-BMW.svg.png"}/>
+                        <span>Performance Motos</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Content;
